1ab539c57fff9c79155e2cdb29601d38967b42ef
Vladislav Sergienko <vlad.sergienko00@yandex.ru>
2022-08-29 19:07:41 +0300

    Test

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+
+#Test
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,10 +124,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-#MERGE THIS#
-#MERGE THIS#
-#MERGE THIS#
-#MERGE THIS#
-#MERGE THIS#
-#MERGE THIS#
